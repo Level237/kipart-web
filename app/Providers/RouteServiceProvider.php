1e6cbc8c8ac6b34267ca9ac305a9c46461a3313e
level237 <Bramslevel129@gmail.com>
2023-01-06 07:57:57 +0100

    init url api

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,6 +35,17 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+                Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+
+            Route::middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/admin.php'));
+
+            Route::middleware('web')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/agent.php'));
         });
     }
 
---
+++ app/services/api/UrlServices.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\services\api;
+
+class UrlServices{
+
+    public static $url="Kipart.stillforce.tech";
+
+    public static function get(){
+        return self::$url;
+    }
+}
