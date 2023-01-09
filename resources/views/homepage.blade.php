fa7c680d5e3a3317d737b1845d0746dbe18e1be1
imagic <aleximagic2020@gmail.com>
2023-01-09 13:44:31 +0100

    imagic

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -154,62 +154,93 @@
 						</div>
 
 						<div class="row mt-3">
-							<div class="col-lg-12 col-md-12 input-border">
+							<div class="col-lg-12 col-md-12">
 
-								<div class="input-group mb-3 ">
+								<div class="input-group mb-1 ">
 
-									<input type="text" class="form-control input-design"  id="basic-url"
+									<input type="text" style="border-radius: 20px" class="form-control input-design"  id="basic-url"
 										aria-describedby="basic-addon3" placeholder="Depart">
+
 								</div>
 							</div>
 						</div>
 						<div class="row mt-3">
 							<div class="col-lg-12 col-md-12">
 
-								<div class="input-group mb-3 ">
+								<div class="input-group  mb-1 ">
 
-									<input type="text" class="form-control input-design" id="basic-url"
+									<input type="text" style="border-radius: 20px" class=" form-control input-design" id="basic-url"
 										aria-describedby="basic-addon3" placeholder="Arrivé">
 								</div>
 							</div>
 						</div>
 
 						<div class="row mt-3">
-							<div class="col-lg-6 col-md-12">
+							<div class="col-lg-6 col-md-12 ">
 
-								<div class="input-group mb-3 ">
+								<div class="input-group mb-1 ">
 
-									<input type="date" class="form-control input-design" id="basic-url"
-										aria-describedby="basic-addon3" placeholder="Date de Depart">
+									<input type="datetime-local" style="border-radius: 20px" class=" input-design" id="basic-url"
+										aria-describedby="basic-addon3" value="Date de Depart">
 								</div>
 							</div>
-							<div class="col-lg-6 col-md-12">
+							<div class="col-lg-6 col-md-12 ">
 
-								<div class="input-group mb-3 ">
+								<div class="input-group mb-1 ">
 
-									<input type="date" class="form-control input-design" id="basic-url"
-										aria-describedby="basic-addon3" placeholder="Ajouter un retour">
+									<input type="datetime-local" style="border-radius: 20px" class=" input-design" id="basic-url"
+										aria-describedby="basic-addon3">
+								</div>
+							</div>
+						</div>
+
+                        <div class="row mt-3">
+							<div class="col-lg-6 col-md-12 ">
+
+								<div class="input-group  ">
+                                    <div class="icon" ><h4 style="color: rgb(229, 145, 0)"><i class="fa fa-user fa-1x"></i>Nombre de Passager</h4></div>
+								</div>
+							</div>
+							<div class="col-lg-6 col-md-12 ">
+
+								<div class="input-group  ">
+                                    <div class="icon" ><h4 style="color: rgb(229, 145, 0)"><i class="fa fa-star fa-1x"></i>Classe</h4></div>
 								</div>
 							</div>
 						</div>
 
 						<div class="row mt-3">
-							<div class="col-lg-6 col-md-12">
+							<div class="col-lg-6 col-md-12 ">
 
-								<div class="input-group mb-3 ">
+								<div class="input-group mb-1 ">
 
-									<input type="number" class="form-control input-design" id="basic-url"
-										aria-describedby="basic-addon3">
+									<input type="number" style="border-radius: 20px" class=" input-design" id="basic-url"
+										aria-describedby="basic-addon3" placeholder="0">
 								</div>
 							</div>
-							<div class="col-lg-6 col-md-12">
+							<div class="col-lg-6 col-md-12 ">
 
-								<div class="input-group mb-3 ">
+								<div class="input-group mb-1 ">
 
-									<input type="text" class="form-control input-design" id="basic-url"
-										aria-describedby="basic-addon3">
+									<select class="input-design input-group" style="align-content: center" >
+                                    <option value="1">VIP</option>
+                                    <option value="2">CLASSIQUE</option>
+                                </select>
 								</div>
 							</div>
+						</div>
+                        <div class="row mt-3" >
+							<div class="col-lg-12 col-md-12" style="align-content: center">
+
+								<div class="form-group button">
+                                    <button type="submit" class="bizwheel-btn3 ">CHERCHER
+                                    </button>
+                                </div>
+
+
+								</div>
+							</div>
+
 						</div>
 					</div>
 					</form>
