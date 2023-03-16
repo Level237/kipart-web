b7586eb4c17f841077ffed702f2c424a0df68fb8
Level237 <Bramslevel129@gmail.com>
2023-03-16 17:12:34 +0100

    update

--- resources/views/search/step-three.blade.php
+++ resources/views/search/step-three.blade.php
@@ -3,6 +3,19 @@
 Choisissez votre Voyage
 @endsection
 
+<style>
+    .reservationBtn{
+        display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: #FFFFFF;
+
+  background: linear-gradient(90deg, rgba(236,154,29,1) 0%, rgba(201,128,16,1) 76%);
+  padding: 1em 1em;
+  border-radius: 2em;
+    }
+</style>
 @section('content')
 
 
@@ -41,32 +54,32 @@ Choisissez votre Voyage
                     <input type="date" name="dateDeparture"  value="{{ $arrayTravel['dateDeparture'] }}"  required  />
                   </div>
                   <div class="input">
-    
+
                     <select name="departure_time" id="" value="{{ $arrayTravel['departure_time'] }}" required>
                         @foreach ($hours as $hour)
                         <option value="{{ $hour->hours }}" @selected($arrayTravel['departure_time'] ==$hour->hours)>{{ $hour->hours }}</option>
                     @endforeach
                     </select>
-    
+
                 </div>
                 </div>
                 <div class="group">
                     <div>
-    
+
                       <div class="input right">
                         <input type="number" value="{{ $arrayTravel['number_of_places']  }}" placeholder="1" id="number-of-passengers" value="1" name="number_of_places" required />
-    
+
                         <button type="button" class="no-bg" >
-    
+
                           <img src="{{asset('assets/images/icon-plus.svg')}}" alt="plus icon" onclick="increaseNumberOfPassengers()"/>
                           <img src="{{asset('assets/images/icon-substract.svg')}}" alt="plus icon" onclick="decreaseNumberOfPassengers()" />
                         </button>
-    
-    
+
+
                       </div>
                     </div>
                     <div>
-    
+
                       <select name="classe" value="{{ $arrayTravel['classe'] }}">
                         <option value="Vip" @selected($arrayTravel['classe'] =="Vip")>VIP</option>
                         <option value="Classique"  @selected($arrayTravel['classe'] =="Classique")>Classique</option>
@@ -98,23 +111,12 @@ Choisissez votre Voyage
               <div class="items">
                 @foreach ($datas as $travel)
 
-                
 
-                
-               
-                    <form method="get" action="{{ route('goto-passenger') }}" id="add">
-                        @csrf
-                        <input type="hidden" name="travel_id" value="{{ $travel->id }}">
-                        <input type="hidden" name="departure" value="{{ $travel->departure }}">
-                        <input type="hidden" name="arrival" value="{{ $travel->arrival }}">
-                        <input type="hidden" name="date" value="{{ $travel->date }}">
-                        <input type="hidden" name="hour" value="{{ $travel->hours }}">
-                        <input type="hidden" name="price" value="{{ $travel->price }}">
-                        <input type="hidden" name="places" value="{{ $travel->number_of_places }}">
-                        <input type="hidden" name="classe" value="{{ $travel->classe }}"> 
-                        <button type="submit" style="border: none">
 
-                            <div class="item">
+
+
+
+                    <div class="item">
                                 <div class="infos">
                                 <div class="header">
                                     <div>{{ $subAgencyName }}</div>
@@ -150,18 +152,39 @@ Choisissez votre Voyage
                                     <div> <a href="#">Notez l'agence</a></div>
                                 </div>
                                 </div>
-                                <div class="price">
-                                <span>{{ $travel->price }}</span>
-                                <span>F CFA</span>
-                                </div>
+                                <form method="get" action="{{ route('goto-passenger') }}" id="add">
+                                    @csrf
+                                    <input type="hidden" name="travel_id" value="{{ $travel->id }}">
+                                    <input type="hidden" name="departure" value="{{ $travel->departure }}">
+                                    <input type="hidden" name="arrival" value="{{ $travel->arrival }}">
+                                    <input type="hidden" name="date" value="{{ $travel->date }}">
+                                    <input type="hidden" name="hour" value="{{ $travel->hours }}">
+                                    <input type="hidden" name="price" value="{{ $travel->price }}">
+                                    <input type="hidden" name="places" value="{{ $travel->number_of_places }}">
+                                    <input type="hidden" name="classe" value="{{ $travel->classe }}">
+
+
+
+
+                                <div class="price" style="">
+                                    <span>{{ $travel->price }} F CFA</span>
+
+                                    </div>
+                                    <div class="price" style="margin-top:18px">
+                                        <button type="submit" style="border: none;background:transparent;color:white;font-size:16px">
+                                            Reserver
+                                        </button>
+
+                                        </div>
+
+                                </form>
                             </div>
-                        </button>
-                    </form>
-                    
 
 
-                        
-                    
+
+
+
+
                 @endforeach
 
 
